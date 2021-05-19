f83612db2a8931a933937597f59e91f5a83ad488
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-05-19 12:49:52 -0500

    make controller object with encoder and lcd

--- pitchshifter_jetson/controls.py
+++ pitchshifter_jetson/controls.py
@@ -6,43 +6,58 @@ import qwiic_twist
 import sys
 import os
 
+class controller:
+
+    #controls
+    transposition = 0
+    window = 0
+    delay = 0
+    resolution = 1
+
+    #constructor
+    def __init__(self):
+
+        #LCD Setup
+        lcd_columns = 16
+        lcd_rows = 2
+
+        #GPIO Setup
+        lcd_rs = digitalio.DigitalInOut(board.D26)
+        lcd_en = digitalio.DigitalInOut(board.D19)
+        lcd_d7 = digitalio.DigitalInOut(board.D27)
+        lcd_d6 = digitalio.DigitalInOut(board.D22)
+        lcd_d5 = digitalio.DigitalInOut(board.D24)
+        lcd_d4 = digitalio.DigitalInOut(board.D25)
+        lcd_backlight = digitalio.DigitalInOut(board.D4)
+
+        #Initialize LCD
+        self.lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+        self.lcd.backlight = True
+
+        #Initialize Encoder
+        self.twist = qwiic_twist.QwiicTwist()
+        if self.twist.connected == False:
+            print("The Qwiic twist device isn't connected to the system. Please check your connection", file=sys.stderr)
+            sys.exit(0)
+        self.twist.begin()
+        self.twist.set_color(255, 0, 0) #set color to red
+
+
 def send2Pd(message=''):
 	os.system("echo '" + message + "' | pdsend 3000")
 
 def main():
 
-    #LCD Setup
-    lcd_columns = 16
-    lcd_rows = 2
+    #Start controller
+    ps = controller()
 
-    #GPIO Setup
-    lcd_rs = digitalio.DigitalInOut(board.D26)
-    lcd_en = digitalio.DigitalInOut(board.D19)
-    lcd_d7 = digitalio.DigitalInOut(board.D27)
-    lcd_d6 = digitalio.DigitalInOut(board.D22)
-    lcd_d5 = digitalio.DigitalInOut(board.D24)
-    lcd_d4 = digitalio.DigitalInOut(board.D25)
-    lcd_backlight = digitalio.DigitalInOut(board.D4)
-
-    #Initialize LCD
-    lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-    lcd.backlight = True
-
-    #Initialize Encoder
-    twist = qwiic_twist.QwiicTwist()
-    if twist.connected == False:
-        print("The Qwiic twist device isn't connected to the system. Please check your connection", file=sys.stderr)
-        return
-    twist.begin()
-    twist.set_color(255, 0, 0) #set color to red
-
-    # Print a two line message
-    lcd.message = "Hello\nWorld"
+    #Print a two line message
+    ps.lcd.message = "Hello\nWorld"
 
     #Loop
     while True:
-        print("Count: %d, Pressed: %s" % (twist.count, "YES" if twist.pressed else "NO",))
-        send2Pd(str(twist.count) + ';')
+        print("Count: %d, Pressed: %s" % (ps.twist.count, "YES" if ps.twist.pressed else "NO",))
+        send2Pd(str(ps.twist.count) + ';')
         time.sleep(.1)
 
 if __name__ == '__main__':
