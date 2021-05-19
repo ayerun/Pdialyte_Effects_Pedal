d119e25f166e0fb2b131d390fa4919087b3739aa
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-05-19 12:09:19 -0500

    made python standard

--- pitchshifter_jetson/lcd_test.py
+++ pitchshifter_jetson/lcd_test.py
@@ -2,23 +2,45 @@ import time
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
 import board
+import qwiic_twist
+import sys
+import os
 
-#LCD Setup
-lcd_columns = 16
-lcd_rows = 2
+def send2Pd(message=''):
+	os.system("echo '" + message + "' | pdsend 3000")
 
-#GPIO Setup
-lcd_rs = digitalio.DigitalInOut(board.D26)
-lcd_en = digitalio.DigitalInOut(board.D19)
-lcd_d7 = digitalio.DigitalInOut(board.D27)
-lcd_d6 = digitalio.DigitalInOut(board.D22)
-lcd_d5 = digitalio.DigitalInOut(board.D24)
-lcd_d4 = digitalio.DigitalInOut(board.D25)
-lcd_backlight = digitalio.DigitalInOut(board.D4)
+def setup():
 
-#Initialize LCD
-lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-lcd.backlight = True
+    #LCD Setup
+    lcd_columns = 16
+    lcd_rows = 2
 
-# Print a two line message
-lcd.message = "Hello\nWorld"
+    #GPIO Setup
+    lcd_rs = digitalio.DigitalInOut(board.D26)
+    lcd_en = digitalio.DigitalInOut(board.D19)
+    lcd_d7 = digitalio.DigitalInOut(board.D27)
+    lcd_d6 = digitalio.DigitalInOut(board.D22)
+    lcd_d5 = digitalio.DigitalInOut(board.D24)
+    lcd_d4 = digitalio.DigitalInOut(board.D25)
+    lcd_backlight = digitalio.DigitalInOut(board.D4)
+
+    #Initialize LCD
+    lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+    lcd.backlight = True
+
+    # Print a two line message
+    lcd.message = "Hello\nWorld"
+
+    return lcd
+
+def main(lcd):
+    sleep(5)
+    lcd.clear()
+
+if __name__ == '__main__':
+	try:
+        lcd = setup()
+		main(lcd)
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("\nEnding Script")
+		sys.exit(0)
