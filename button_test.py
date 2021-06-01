b8a37200f642937f45c8db4d99e6ac7d0d9f8137
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-06-01 12:36:51 -0500

    deinitializing pin now

--- button_test.py
+++ button_test.py
@@ -3,16 +3,16 @@ import board
 import time
 import sys
 
-def main():
-    pin = digitalio.DigitalInOut(board.D8)
-    pin.switch_to_input()
+def main(pin):
     while True:
         print(pin.value)
         time.sleep(0.1)
 
 if __name__ == '__main__':
 	try:
-		main()
+        pin = digitalio.DigitalInOut(board.D8)
+        main(pin)
 	except (KeyboardInterrupt, SystemExit) as exErr:
-		print("\nEnding Script")
-		sys.exit(0)
+        pin.deinit()
+        print("\nEnding Script")
+        sys.exit(0)
