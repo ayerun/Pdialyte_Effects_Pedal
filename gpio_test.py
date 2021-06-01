fc0e48a8d54155e75c011dbbac8fc407f74e744c
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-06-01 13:07:08 -0500

    made button into interrupt

--- gpio_test.py
+++ gpio_test.py
@@ -1,19 +1,25 @@
-import RPi.gpio as GPIO
+import RPi.GPIO as GPIO
 import time
 
 button = 8
 
+def button_back(channel):
+    print("Button Pressed!")
+
+
+
 def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(button, GPIO.IN)
+    GPIO.add_event_detect(button, GPIO.FALLING, callback=button_back, bouncetime=10)
 
     try:
         while True:
-            GPIO.wait_for_edge(button, GPIO.FALLING)
-            print("Button Pressed!")
-            GPIO.cleanup()
+            print("nothing happening")
+            time.sleep(0.1)
     finally:
         GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
+    GPIO.cleanup()
